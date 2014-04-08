fa2f3b6f9e7916b7b7f9a8c1fe3a7c7178a620d9
Rihan Pereira <rihen234@gmail.com>
2014-04-08 10:23:55 +0530

    code refactored. added passport support

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var express = require('express');
+var passport = require('passport');
+var configDB = require('./lib/config/database');
 
 /**
  * Main application file
@@ -14,11 +16,14 @@ var config = require('./lib/config/config');
 
 var app = express();
 
-// Express settings
+//passport configuration
+require('./lib/config/passport')(passport);
+
+//Express settings
 require('./lib/config/express')(app);
 
 // Routing
-require('./lib/routes')(app);
+require('./lib/routes').configRoutes(app, passport);
 
 // Start server
 app.listen(config.port, function () {
